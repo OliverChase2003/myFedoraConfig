42919d5c30d6e9e8fbd1a2615cb23572a9038230
OliverChase2003 <oliverchase5408@gmail.com>
2025-04-17 10:14:29 +0800

    gnome-desktop inst test

---
+++ gnome-desktop.sh
@@ -0,0 +1,6 @@
+sudo dnf install @gnome-desktop --exclude=\
+gnome-maps,gnome-software,gnome-calendar,\
+gnome-clocks,gnome-connections,gnome-contacts,\
+gnome-software,gnome-text-editor,gnome-user-docs,\
+gnome-weather,totem,gnome-logs,gnome-calculator,\
+gnome-boxes,flatpak
