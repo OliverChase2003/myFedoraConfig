16958d3a758d9d3b514f4aceb516b05dd1d6961a
OliverChase2003 <oliverchase5408@gmail.com>
2025-04-17 12:02:06 +0800

    gnome-desktop inst last maybe

--- desktop_inst.sh
+++ desktop_inst.sh
@@ -9,44 +9,34 @@ sudo dnf install -y \
 echo "更新系统..."
 sudo dnf update -y
 
-echo "配置命令行工具"
-## neovim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-
-## fish
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher install PatrickF1/fzf.fish
-
-## zsh
-
-echo "安装最小 GNOME 核心组件..."
-sudo dnf install -y \
-  gdm \
-  gnome-shell \
-  gnome-session \
-  mutter \
-  gnome-settings-daemon \
-  gnome-keyring \
-  gnome-backgrounds \
-  adwaita-icon-theme \
-  liberation-fonts \
-  fontconfig \
-  gvfs \
-  xdg-user-dirs \
-  gsettings-desktop-schemas \
-  gnome-browser-connector
+sudo dnf install @gnome-desktop --exclude=\
+baobab,\
+evince,\
+evince-djvu,\
+gnome-calculator,\
+gnome-calendar,\
+gnome-characters,\
+gnome-clocks,\
+gnome-color-manager,\
+gnome-contacts,\
+gnome-epub-thumbnailer,\
+gnome-font-viewer,\
+gnome-logs,\
+gnome-maps,\
+gnome-tour,\
+gnome-user-docs,\
+gnome-user-share,\
+gnome-weather,\
+loupe,\
+simple-scan,\
+snapshot,\
+totem,\
+yelp,\
+gnome-boxes,\
+gnome-connections,\
+gnome-initial-setup,\
+gnome-software,\
+gnome-text-editor,\
+flatpak-libs
 
 sudo systemctl set-default graphical.target
-
-echo "安装必要的 GNOME 桌面应用"
-sudo dnf install -y \
-  ptyxis \
-  nautilus \
-  gnome-control-center \
-  gnome-tweaks \
-  gnome-extensions-app \
-  firefox
-
-## remove gnome help
-sudo dnf remove  -y \
-  gnome-tour
